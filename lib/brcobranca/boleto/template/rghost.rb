9fc2c1d5b0fe8b315bf81393ad61eb5dd8cefd37
Lailson Henrique <lailsonh@hotmail.com>
2015-09-25 01:10:36 -0300

    Update rghost.rb
    Alterando posicao cedente

--- lib/brcobranca/boleto/template/rghost.rb
+++ lib/brcobranca/boleto/template/rghost.rb
@@ -141,7 +141,7 @@ module Brcobranca
           doc.show "#{boleto.banco}-#{boleto.banco_dv}", :tag => :grande
           doc.moveto :x => '7.5 cm' , :y => '23.85 cm'
           doc.show boleto.codigo_barras.linha_digitavel, :tag => :grande
-          doc.moveto :x => '0.7 cm' , :y => '23 cm'
+          doc.moveto :x => '0.8 cm' , :y => '23 cm'
           doc.show boleto.cedente
           #doc.moveto :x => '11 cm' , :y => '23 cm'
           #doc.show boleto.agencia_conta_boleto
